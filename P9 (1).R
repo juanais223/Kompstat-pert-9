b91e97dda52dbd0e60c12aa8458f6f0a1abc6854
J. AISYA <juanais223@gmail.com>
2026-05-11 12:37:18 +0700

    apa aja

--- P9 (1).R
+++ P9 (1).R
@@ -1,15 +1,11 @@
 summary(mtcars)
 mean(mtcars$mpg)
 sd(mtcars$mpg)
-max(mtcars$mpg)
 
-<<<<<<< HEAD
 hist(mtcars)
 boxplot(mtcars)
 
 hist(mtcars$cyl)
 boxplot(mtcats$cyl)
-=======
-hist(mtcars$mpg)
-boxplot(mtcars$cyl)
->>>>>>> branch-baru
+
+barplot(mtcars$cyl)
